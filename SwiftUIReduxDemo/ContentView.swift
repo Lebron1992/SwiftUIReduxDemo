5bdbee2b2a7300be9ccfa46c42d99d602fbd38bb
Lebron <wenzhi.zeng@outlook.com>
2022-04-23 16:09:13 +0800

    Initial Commit

---
+++ SwiftUIReduxDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIReduxDemo
+//
+//  Created by Lebron Zeng on 2022/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIReduxDemo/SwiftUIReduxDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReduxDemoApp.swift
+//  SwiftUIReduxDemo
+//
+//  Created by Lebron Zeng on 2022/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReduxDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
